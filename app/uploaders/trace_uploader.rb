6651b3d186ad470317f109b6008a92a5f579cf2e
flowkater <chjw102@gmail.com>
2013-11-15 08:29:15 +0900

    cerberus jar replace

--- app/uploaders/trace_uploader.rb
+++ app/uploaders/trace_uploader.rb
@@ -14,8 +14,4 @@ class TraceUploader < CarrierWave::Uploader::Base
     "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def extension_white_list
-    %w(trace)
-  end
-
 end

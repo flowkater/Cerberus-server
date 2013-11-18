3fa4378d2e9e2f2f72bd89ecd5f1f37d6321323e
flowkater <chjw102@gmail.com>
2013-11-18 15:27:19 +0900

    tree re update

--- app/models/leak_instance.rb
+++ app/models/leak_instance.rb
@@ -8,20 +8,16 @@ class LeakInstance < ActiveRecord::Base
   accepts_nested_attributes_for :children
   attr_accessible :instancename, :instanceid, :size, :leak_suspect, :parent_id, :children_attributes
 
-  # acts_as_tree
+  acts_as_tree
 
   def hash_to_html(hash = hash_tree, level=0)
     result = []
     hash.each do |key,value|
-      result << "<tr><td><div style='margin-left:#{(level * 20)}px;'>#{key.instancename}</div></td><td></td><td></td></tr>"
+      result << "<tr><td><div style='margin-left:#{(level * 20)}px;'>#{key.instancename}</div></td></tr>"
       result << hash_to_html(value, level + 1) if value.is_a?(Hash)
     end
 
    result.join("")
-  end
-
-  def leak_tree_html
-    ""
   end
 
   def parent_level(count = 0)

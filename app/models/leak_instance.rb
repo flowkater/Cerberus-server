37588bc4af57f74cec72056c8e969ed1cc0c1965
AndyHwang <hwanghakbeom@gmail.com>
2013-11-18 19:08:18 +0900

    change little things

--- app/models/leak_instance.rb
+++ app/models/leak_instance.rb
@@ -13,7 +13,26 @@ class LeakInstance < ActiveRecord::Base
   def hash_to_html(hash = hash_tree, level=0)
     result = []
     hash.each do |key,value|
-      result << "<tr><td><div style='margin-left:#{(level * 20)}px;'>#{key.instancename}</div></td><td></td><td></td></tr>"
+      classname = 'badge' if level == 0
+      classname = 'badge badge-red' if level == 1
+      classname = 'badge badge-gray' if level == 2
+      classname = 'badge badge-green' if level == 3
+      classname = 'badge badge-blue' if level == 4
+      classname = 'badge badge-dark-red' if level == 5
+      classname = 'badge badge-black' if level == 6
+      classname = 'badge badge-cyan' if level == 7
+      classname = 'badge badge-purple' if level == 8
+      classname = 'badge' if level == 9
+      classname = 'badge badge-red' if level == 10
+      classname = 'badge badge-purple' if level == 11
+      classname = 'badge badge-gray' if level == 12
+      classname = 'badge badge-green' if level == 13
+      classname = 'badge badge-blue' if level == 14
+      classname = 'badge badge-dark-red' if level == 15
+      classname = 'badge badge-black' if level == 16
+      classname = 'badge badge-cyan' if level == 17
+      classname = 'badge badge-purple' if level == 18
+      result << "<tr><td><div style='margin-left:#{(level * 20)}px;'><span class='"+classname+"'>#{(level)}</span>&nbsp;#{key.instancename}</div></td><td></td><td></td></tr>"
       result << hash_to_html(value, level + 1) if value.is_a?(Hash)
     end
 
